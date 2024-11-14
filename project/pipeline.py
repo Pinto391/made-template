f2ceff9090de4091725d9537d5a00d395c04f444
Pinto391 <81520171+Pinto391@users.noreply.github.com>
2024-11-14 11:29:27 +0100

    Update pipeline.py

--- project/pipeline.py
+++ project/pipeline.py
@@ -10,7 +10,7 @@ import requests
 import zipfile
 
 # Constants
-DATA_URL = "https://www.kaggle.com/api/v1/datasets/download/pinto391/380000-weather-data"  # Replace with your dataset's URL
+DATA_URL = "https://www.kaggle.com/api/v1/datasets/download/pinto391/380000-weather-data" 
 DATA_DIR = "./data"
 ZIP_FILE_PATH = os.path.join(DATA_DIR, "dataset.zip")
 OUTPUT_FILE = os.path.join(DATA_DIR, "cleaned_data.csv")
@@ -56,16 +56,17 @@ print("Cleaning data...")
 df.dropna(inplace=True)
 
 # - Convert date columns to datetime format
-if 'date' in df.columns:
-    df['date'] = pd.to_datetime(df['date'], errors='coerce')
-    df.dropna(subset=['date'], inplace=True)
+if 'date_time' in df.columns:
+    df['date_time'] = pd.to_datetime(df['date_time'], errors='coerce')
+    df.dropna(subset=['date_time'], inplace=True)
 
 
 # Assuming there's a 'temperature' column where values below -50 or above 60 are considered errors
-if 'temperature' in df.columns:
-    df = df[(df['temperature'] >= -50) & (df['temperature'] <= 60)]
-
-
+if 'maxtempC' in df.columns:
+    df = df[(df['maxtempC'] <= 60)]
+    
+if 'mintempC' in df.columns:
+    df = df[(df['mintempC'] >= -50)]
 # Save the Cleaned Data
 print("Saving cleaned data...")
 df.to_csv(OUTPUT_FILE, index=False)
